c7926a4eb1e0372530e97a9722ded84fb2328617
bahaeldin <bahaa.ahmed.eng@gmail.com>
2026-03-08 12:27:57 -0700

    adds changes to fomhandler file

--- formhandler.php
+++ formhandler.php
@@ -1,63 +1,20 @@
-
 <?php
 
-// header('Location:thx.html');
 ob_start();
 
 $name = '';
 $email = '';
 $gender = '';
 $wines = '';
+$gender = '';
 $regions = '';
 $comments = '';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    if(isset($_POST['name'],
-    $_POST['email'],
-    $_POST['gender'],
-    $_POST['wines'],
-    $_POST['regions'],
-    $_POST['comments'])) {
-
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $gender = $_POST['gender'];
-        $wines = $_POST['wines'];
-        $regions = $_POST['regions'];
-        $comments = $_POST['comments'];
-
-        function my_wines($wines) {
-            $my_return = '';
-            if(!empty($wines)) {
-                $my_return = implode(', ', $wines);
-            }
-            return $my_return;
-        }
-
-        $to = 'myname@gmail.com';
-        $subject = 'Test Email on ' .date('m/d/y, h i A');
-        $body = '
-Name : '.$name.'  '.PHP_EOL.'
-Email : '.$email.'  '.PHP_EOL.'
-Gender : '.$gender.'  '.PHP_EOL.'
-Region : '.$regions.'  '.PHP_EOL.'
-Wines : '.my_wines($wines).'  '.PHP_EOL.'   
-Comments : '.$comments.'  '.PHP_EOL.'
-';
-
-        // Fixed headers format for mail() function
-        $headers = 'From: noreply@mystudentswa.com';
-
-        // Fixed logical condition - removed extra parentheses and used proper AND operator
-        if(!empty($name) && !empty($email) && !empty($gender) && !empty($wines) && !empty($regions) && !empty($comments)) {
-            mail($to, $subject, $body, $headers);
-            header('Location:thx.html');
-        }
-    }
-}
-
-/* ?>$_POST['gender'],
+if(isset($_POST['name'],
+$_POST['email'],
+$_POST['gender'],
 $_POST['wines'],
 $_POST['regions'],
 $_POST['comments'])) {
@@ -78,7 +35,7 @@ return $my_return;
 }
 
 
-$to = 'myname@gmail.com';
+$to = 'bahaa.ahmed.eng@gmail.com';
 $subject = 'Test Email on ' .date('m/d/y, h i A');
 $body = '
 Name : '.$name.'  '.PHP_EOL.'
@@ -93,11 +50,11 @@ $headers = array(
 'From' => 'noreply@mystudentswa.com'  
 );
 
-if(!empty($name && $email && $gender && $wines && $regions && $comments)) {
+if(!empty($name) && !empty($email) && !empty($gender) && !empty($wines) && !empty($regions) && !empty($comments))  {
 
 mail($to, $subject, $body, $headers);
 header('Location:thx.html');
-
+exit;
 }
 
 
@@ -105,4 +62,4 @@ header('Location:thx.html');
 
 
 
-}*/
+}
